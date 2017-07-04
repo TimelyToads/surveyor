99486edc6393fd44cecbae36cff6be7fa895b099
Jun Park <jun.park@live.com>
2017-07-04 14:15:19 -0700

    Add api_keys.js for Watson

---
+++ server/externals/api_keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  nlu_url: "https://gateway.watsonplatform.net/natural-language-understanding/api",
+  nlu_username: "6c0c4c52-1fff-459f-b118-17399e8df111",
+  nlu_password: "wyosCahIxS0l"
+}
