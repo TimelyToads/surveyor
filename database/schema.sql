628f699554c777dc376b7a494fe0089de6981371
Kirk Rohani <kirk.rohani@gmail.com>
2017-07-04 12:15:42 -0700

    intial db setup instructions

---
+++ database/schema.sql
@@ -0,0 +1,14 @@
+psql postgres
+DROP DATABASE IF EXISTS surveyordb;
+DROP USER IF EXISTS dbuser;
+CREATE ROLE dbuser with LOGIN PASSWORD 'password';
+ALTER ROLE DBUSER CREATEDB;
+\q
+psql postgres -U dbuser;
+CREATE DATABASE surveyorDB;
+GRANT ALL PRIVILEGES ON DATABASE surveyorDB TO dbuser;
+\connect surveyordb;
+CREATE TABLE users (id int primary key not null, facebook_id char(50) not null);
+CREATE TABLE resumes(id int primary key not null, user_id int REFERENCES users(id));
+\dt
+
