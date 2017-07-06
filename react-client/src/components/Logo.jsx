35a59c7103fca622d5e72f49aa1ffde77251890b
Kirk Rohani <kirk.rohani@gmail.com>
2017-07-06 10:12:10 -0700

    added dice logo

--- react-client/src/components/Logo.jsx
+++ react-client/src/components/Logo.jsx
@@ -1,22 +1,41 @@
-import { Label, Image } from 'semantic-ui-react'
+import { Label, Image } from 'semantic-ui-react';
+import React from 'react';
 
-function Logo(props) {
-
-  if (props.jobListItem.jobSourceWebsite === 'Indeed') {
-    return (
-      <Label color="blue" as='a' image>
-        <Image src='images/indeed.com-logo.png' />
-        {props.jobListItem.jobSourceWebsite}
-      </Label>
-    )
-  } else if (props.source === 'Dice') {
-    return (
-        <Label color="red" as='a' image>
-        <Image src='images/dice_logo.png' />
-        {this.props.jobListItem.jobSourceWebsite}
-      </Label>
-    )
+class Logo extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  	
+
+  render() {
+    if (this.props.jobListItem.jobSourceWebsite === 'Indeed') {
+      return (
+      <div>
+        <Label color="blue" as='a' image>
+          <Image src='images/indeed.com-logo.png' />
+          {this.props.jobListItem.jobSourceWebsite}
+        </Label>
+      </div>
+      )
+    } else if (this.props.jobListItem.jobSourceWebsite === 'Dice') {
+      return (
+      <div>
+        <Label color="red" as='a' image>
+          <Image src='images/dice_logo.jpg' />
+          {this.props.jobListItem.jobSourceWebsite}
+        </Label>
+      </div>
+      )
+    }
+  } //end render
 }
-export default Logo;
+export default Logo
+
+
+  // } else if (props.jobListItem.jobSourceWebsite  === 'Dice') {
+  //   return (
+  //       <Label color="red" as='a' image>
+  //       <Image src='images/dice_logo.png' />
+  //       {this.props.jobListItem.jobSourceWebsite}
+  //     </Label>
+  //   )
+  // }
