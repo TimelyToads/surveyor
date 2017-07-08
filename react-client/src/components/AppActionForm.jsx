db935958f47219d1b2a1a329b58dc22f8ce1d9b0
Dylan Gould <dylangood@gmail.com>
2017-07-08 16:02:10 -0700

    Complete hookup of Schedule an Action button

--- react-client/src/components/AppActionForm.jsx
+++ react-client/src/components/AppActionForm.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Checkbox, Form, Input, Select } from 'semantic-ui-react'
-import axios from 'axios'
 
 const options = [
-  { key: 0, text: 'Follow-up Call', value: 'followUpCall' },
-  { key: 1, text: 'Follow-up Email', value: 'followUpEmail' },
-  { key: 2, text: 'Phone Screen', value: 'phoneScreen' },
-  { key: 3, text: 'Phone Interview', value: 'phoneInterview' },
-  { key: 4, text: 'Teleconference', value: 'teleconference' },
-  { key: 5, text: 'Onsite Interview', value: 'onsiteInterview' },
+  { key: 0, text: 'Follow-up Call', value: 'Follow-up Call' },
+  { key: 1, text: 'Follow-up Email', value: 'Follow-up Email' },
+  { key: 2, text: 'Phone Screen', value: 'Phone Screen' },
+  { key: 3, text: 'Phone Interview', value: 'Phone Interview' },
+  { key: 4, text: 'Video Conference', value: 'Video Conference' },
+  { key: 5, text: 'Onsite Interview', value: 'Onsite Interview' },
   { key: 6, text: 'Job Test', value: 'Job Test' },
-  { key: 7, text: 'Informational Interview', value: 'informationalInterview' },
+  { key: 7, text: 'Informational Interview', value: 'Informational Interview' },
 ];
 
 class AppActionForm extends React.Component {
@@ -18,43 +17,42 @@ class AppActionForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: '2017-08-11',
-      type: 'Followup Email',
-      contact: 'some@body.com',
+      date: '',
+      type: '',
+      contact: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(e) {
-    //
+  handleChange (e, { name, value }) {
+    let newActionObj = this.state;
+    newActionObj[[name]] = value;
+    this.setState(newActionObj);
   }
 
   handleSubmit(e) {
     var action = {
-      username: this.props.username,
-      job_id: this.props.jobId,
       date: this.state.date,
       type: this.state.type,
       contact: this.state.contact,
       completed: false
     };
-    axios.post(`/api/users/${this.props.username}/jobs/action`, action)
-    .then(res => {
-      console.log('Created new action in DB: ', action);
-    })
-    .catch(err => {
-      console.log('ERROR creating new action in DB: ', err);
-      alert('Database error! New action was not saved.');
+    this.setState({
+      date: '',
+      type: '',
+      contact: ''
     });
+    this.props.handleSubmit(action);
   }
 
   render() {
     return (
       <Form>
         <Form.Group widths='equal'>
-          <Form.Input label='Due Date' placeholder={'2001-01-01'} />
-          <Form.Select label='Action to Take' options={options} placeholder='action' />
-          <Form.Input label='Contact Info' placeholder='contact info' />
+          <Form.Input label='Due Date' name={'date'} value={this.state.date} onChange={this.handleChange} placeholder={'2001-01-01'} />
+          <Form.Select label='Action to Take' name={'type'} value={this.state.type} options={options} onChange={this.handleChange} placeholder='action' />
+          <Form.Input label='Contact Info' name={'contact'} value={this.state.contact} onChange={this.handleChange} placeholder='contact info' />
         </Form.Group>
         <Form.Button onClick={this.handleSubmit} fluid color='blue' >Schedule an Action</Form.Button>
       </Form>
